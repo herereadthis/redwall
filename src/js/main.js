3fd7c856eb9e1d7156dc71529d170f243311ceb8
Jimmy Ha <herereadthis@gmail.com>
2014-05-28 22:23:26 -0400

    social links now function #2

--- src/js/main.js
+++ src/js/main.js
@@ -12,15 +12,17 @@
         // },
         paths: {
             "ColorTuner": "color_tuner",
+            "SocialFu": "social_fu",
             "Analytics": "analytics",
             jquery: ['https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min', "lib/jquery.min"]
         }
     });
-    require(["jquery", "ColorTuner", "Analytics"], function($, ColorTuner, Analytics) {
+    require(["jquery", "ColorTuner", "SocialFu", "Analytics"], function($, ColorTuner, SocialFu, Analytics) {
         // if (Modernizr.touch === false) {
         //       HeadMore.init();
         // }
         ColorTuner.init();
+        SocialFu.init();
         return Analytics.track();
     });
 }).call(this);
