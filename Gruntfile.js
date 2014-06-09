bc88a0293b8f32a274f7e9ead11b27b6d886726c
Jimmy Ha <herereadthis@gmail.com>
2014-06-09 00:57:15 -0400

    just some upkeep #7

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
         watch: {
             // runs less task when any less files change
             less: {
-                files: ["./src/less/*", "./bellmaker/src/less/*"],
+                files: ["./src/less/*", "./bellmaker/src/less/*", , "./bellmaker/src/demo/less/*"],
                 tasks: ["less"]
             },
             html: {
