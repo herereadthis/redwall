3bf68b06a3fd579c38de7ff0dac316d2fac360d9
Jimmy Ha <herereadthis@gmail.com>
2014-06-25 19:03:33 -0400

    creates footer js dependency #30

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,17 @@
             "ColorTuner": "color_tuner",
             "SocialFu": "social_fu",
             "Analytics": "analytics",
+            "CodeFooter": "code_footer",
             "jquery": '../components/jquery/dist/jquery.min'
         }
     });
-    require(["jquery", "ColorTuner", "SocialFu", "Analytics"], function($, ColorTuner, SocialFu, Analytics) {
+    require(["jquery", "ColorTuner", "SocialFu", "Analytics", "CodeFooter"], function($, ColorTuner, SocialFu, Analytics, CodeFooter) {
         // if (Modernizr.touch === false) {
         //       HeadMore.init();
         // }
         ColorTuner.init();
         SocialFu.init();
+        CodeFooter.init();
         return Analytics.track();
     });
 }).call(this);
