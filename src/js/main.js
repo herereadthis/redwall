9842790ae0e1f69f8a763be1b6d09a88a43bcfe8
Jimmy Ha <herereadthis@gmail.com>
2014-07-30 00:42:59 -0400

    completes parallax scroll #55

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,7 @@
             'Retromator': 'retromator',
             "ColorTuner": "color_tuner",
             "SocialFu": "social_fu",
-            "ParallaxScroll": "parallax_scroll",
-            // "ParallaxScroll": "../components/greeneyes/src/js/parallax_scroll",
+            "ParallaxScroll": "../components/greeneyes/src/js/parallax_scroll",
             // coding blog
             'CodeBlog': 'code_blog',
             "CodeFooter": "code_footer",
