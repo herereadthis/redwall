397d3d6340705f6755f433e4e35c550efcc283fd
Jimmy Ha <herereadthis@gmail.com>
2014-08-10 02:58:45 -0400

    rough cut hit counter #63

--- src/js/hit_counter.js
+++ src/js/hit_counter.js
@@ -1,6 +1,6 @@
 (function() {
-    define(['jquery'],
-        function($) {
+    define(['jquery', 'PageStats'],
+        function($, PageStats) {
         var exports, gVars, makeItHappen, dVars, moduleName, makeNumbers, addNumberCell;
         exports = {};
         gVars = {
@@ -9,24 +9,44 @@
         dVars= {};
         moduleName = 'hit_counter';
         _window = $(window);
-        addNumberCell = function ($this, placeNumber) {
-            console.log(placeNumber);
+        addNumberCell = function($this, placeNumber) {
             var placeNumberClass = 'counter_digit_' + placeNumber;
             $counterContainer = $this.find('#' + moduleName);
+            var hitNum = dVars.hitArray;
             $counterContainer.prepend($('<div />').addClass(placeNumberClass).html(0));
         };
-        makeNumbers = function($this) {
-            var _i, _len;
+        makeNumbers = function($this, pageHits) {
+            var _i, _len, pageHits;
             _len = dVars.figures;
-            console.log('makeNumbers');
             for (_i = 0; _i < _len; _i++) {
                 addNumberCell($this, _i);
             }
-
         };
         makeItHappen = function($this) {
             dVars.figures = parseInt($this.data('hit-counter-figures'), 10) || gVars.figures;
-            makeNumbers($this);
+            var canonical = $('[rel="canonical"]').attr('href');
+            var jsonFile = 'http://redwall.herereadthis.com/api/page_stats/?url=' + canonical;
+            makeNumbers($this, dVars.figures);
+            $.ajax({
+                url: jsonFile,
+                type: 'get',
+                dataType: 'json'
+            }).done(function(data) {
+                dVars.pageHits = parseInt(data.page_hits, 10);
+                dVars.hitString = dVars.pageHits.toString();
+                dVars.hitSize = dVars.hitString.length;
+                dVars.hitArray = dVars.hitString.split('');
+                for (num in dVars.hitArray) {
+                    dVars.hitArray[num] = parseInt(dVars.hitArray[num], 10);
+                }
+                console.log(dVars.hitArray.length);
+                dVars.hitArray.reverse();
+                $counterContainer = $this.find('#' + moduleName);
+                for (num in dVars.hitArray) {
+                var placeNumberClass = '.counter_digit_' + num;
+                    $counterContainer.find(placeNumberClass).html(dVars.hitArray[num]);
+                }
+            });
         };
         exports.init = function($this) {
             var element;
