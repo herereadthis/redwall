9e6f2a533fe89d383edbd4b00ea8c2da2004445f
herereadthis <herereadthis@gmail.com>
2014-12-13 00:57:38 -0500

    #123 social fu ng-repeat

--- app/scripts/components/socialFu/socialFu.js
+++ app/scripts/components/socialFu/socialFu.js
@@ -11,10 +11,37 @@ angular.module('redwallApp')
                 '$scope',
                 function ($scope) {
                     window.console.log('social fu');
+
+                    $scope.socialData = [
+                        {
+                            resourceUrl: 'https://www.facebook.com/',
+                            text: 'Facebook',
+                            url: 'https://www.facebook.com/herereadthis',
+                            account: 'herereadthis'
+                        },
+                        {
+                            resourceUrl: 'https://twitter.com/',
+                            text: 'Twitter',
+                            url: 'http://twitter.com/herereadthis',
+                            account: 'herereadthis'
+                        },
+                        {
+                            resourceUrl: 'https://github.com/',
+                            text: 'GitHub',
+                            url: 'https://github.com/herereadthis/redwall',
+                            account: 'herereadthis'
+                        },
+                        {
+                            resourceUrl: 'http://pinterest.com/',
+                            text: 'Pinterest',
+                            url: 'http://pinterest.com/herereadthis/',
+                            account: 'herereadthis'
+                        }
+                    ]
                 }],
             link: function (scope, element) {
             },
-            templateUrl: '/scripts/components/popupBoxSimulator/popup-box-simulator.html'
+            templateUrl: '/scripts/components/socialFu/social-fu.html'
         };
     });
 
