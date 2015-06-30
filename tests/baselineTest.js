fcb567685d5859dc23214daf1b4612e035e8cc2e
herereadthis <herereadthis@gmail.com>
2015-06-30 01:06:07 -0400

    #144 unit test fooling around

--- tests/baselineTest.js
+++ tests/baselineTest.js
@@ -2,12 +2,14 @@ var jsdom = require('mocha-jsdom');
 var assert = require("assert");
 var expect = require('chai').expect;
 
+
+
 describe('baseline mocha tests', function () {
 
     jsdom();
 
     it('has document', function () {
-        var elemID = 'new_element';
+        var elemID = 'app';
         var elemText = 'Hello World';
 
         var elem = document.createElement('div');
@@ -24,6 +26,14 @@ describe('Array', function () {
         it('should return -1 when the value is not present', function () {
             assert.equal(-1, [1, 2, 3].indexOf(5));
             assert.equal(-1, [1, 2, 3].indexOf(0));
+        })
+    });
+    describe('Length', function () {
+        it('should return 2 for array of 2 elements', function () {
+            assert.equal(2, [1, 2].length);
+        })
+        it('should return 5 for an array of 5 elements', function () {
+            assert.equal(5, [1, 2, 3, 4, 5].length);
         })
     })
 });
