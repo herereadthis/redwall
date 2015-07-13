d919ea607f27ce4c04a7908871550ac49a1da507
herereadthis <herereadthis@gmail.com>
2015-07-12 23:58:23 -0400

    #157 buttons for nineties image

--- app/views/Homepage/components/NinetiesImgBox.js
+++ app/views/Homepage/components/NinetiesImgBox.js
@@ -22,10 +22,20 @@ export default class NinetiesImgBox extends React.Component {
         }, true);
     }
 
+    closeNinetiesBox = () => {
+        this.props.flux.getActions(HomeActions.ID).showNinetiesImgBox(false);
+    };
+
     render() {
         return (
             <div className="nineties_img_box">
                 <div className="bellmaker_container">
+                    <div className="nineties_img_header">
+                        <div className="nineties_img_head_button mac_os8_sprites close" onClick={this.closeNinetiesBox}></div>
+                        <div className="nineties_img_head_button mac_os8_sprites resize" onClick={this.closeNinetiesBox}></div>
+                        <div className="nineties_img_head_button mac_os8_sprites minimize" onClick={this.closeNinetiesBox}></div>
+                        <div className="nineties_img_head_text mac_os8_sprites pictures" onClick={this.closeNinetiesBox}></div>
+                        </div>
                     <div className="nineties_img_container">
                         <p>Nineties Image Box!!!!</p>
                     </div>
