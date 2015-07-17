04c7877a239e47f4c1f950d686adc2c2003449f7
herereadthis <herereadthis@gmail.com>
2015-07-17 00:22:08 -0400

    #162 makes image index

--- app/views/Homepage/views/NinetiesImageIndex.js
+++ app/views/Homepage/views/NinetiesImageIndex.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import HomeActions from 'views/Homepage/HomeActions.js';
+import AppRoutes from 'AppRoutes.js';
 
 export default class NinetiesImageIndex extends React.Component {
 
     constructor() {
         super();
     }
+
+    static contextTypes = {
+        router: React.PropTypes.func
+    };
 
     componentWillMount() {
         this.props.flux.getActions(HomeActions.ID).set90slist();
@@ -14,25 +19,50 @@ export default class NinetiesImageIndex extends React.Component {
     componentDidMount() {
     }
 
-    //shouldComponentUpdate(nextProps) {
-    //    var toUpdate = false
-    //    //return nextProps.listing !== this.props.listing;
-    //
-    //    if (nextProps.listing !== this.props.listing ||
-    //        nextProps.listing !== this.props.listing ||
-    //        nextProps.listing !== this.props.listing) {
-    //        window.console.log('asdf');
-    //    }
-    //}
+    handleClick = (uniqueID) => {
+        this.context.router.transitionTo(AppRoutes.NINETIES_IMG, {id: uniqueID});
+    };
+
+    renderListing = () => {
+        return this.props.listing.map((value, key) => {
+            return (
+                <li key={key}
+                    id={`listing-${value.uniqueID}`}
+                    onClick={this.handleClick.bind(this, value.uniqueID)}>
+                    <div>
+                        <img src={value.thumbnail}/>
+                    </div>
+                    <p>{value.title}</p>
+                </li>
+            );
+        });
+    };
+
+    renderIndex = () => {
+        return (
+            <ul className="clear_floats">
+                {this.renderListing()}
+            </ul>
+        );
+    };
+
 
     render() {
         window.console.log(this.props.listing);
-
-        return (
-            <div>
-                <p>Hello Nineties Image, Here be the Index</p>
-            </div>
-        );
+        if (this.props.listing === undefined) {
+            return (
+                <div>
+                    <p>Loading...</p>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className="ninties_img_listing">
+                    {this.renderIndex()}
+                </div>
+            );
+        }
     }
 }
 
