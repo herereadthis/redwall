8ec3fdc2de562f785b99bbb3dcb891540e966a73
herereadthis <herereadthis@gmail.com>
2015-07-19 22:52:40 -0400

    #163 scroll set height

--- app/views/Homepage/views/NinetiesImage.js
+++ app/views/Homepage/views/NinetiesImage.js
@@ -17,7 +17,10 @@ export default class NinetiesImage extends React.Component {
     }
 
     componentDidMount() {
-        var _this = this;
+        var _this;
+
+        _this = this;
+
         this.props.flux.getActions(HomeActions.ID).set90sNavRoutes(
             this.props.dataCount, this.props.data.pk);
 
@@ -29,7 +32,36 @@ export default class NinetiesImage extends React.Component {
                 _this.handleClick('next');
             }
         }, true);
+
+        window.addEventListener('resize', function () {
+            _this.killResizeListener();
+            _this.setScrollContainerHeight();
+        }, true);
+
+
+
     }
+
+    componentWillUnmount() {
+        this.killResizeListener();
+    }
+
+
+    killResizeListener = () => {
+        window.removeEventListener('resize', this.setScrollContainerHeight(),
+            true);
+    };
+
+    setScrollContainerHeight = () => {
+        var scrollBoxContainer, scrollBoxParentHeight;
+        scrollBoxContainer = React.findDOMNode(this.refs.scrollBoxContainer);
+        scrollBoxParentHeight = scrollBoxContainer.parentNode.parentNode.offsetHeight;
+
+        scrollBoxContainer.style.height = `${scrollBoxParentHeight - 45}px`;
+
+        window.console.log(scrollBoxParentHeight);
+
+    };
 
     handleClick = (increment) => {
         var {router} = this.context;
@@ -49,40 +81,48 @@ export default class NinetiesImage extends React.Component {
     }
 
     render() {
-        var style = {
-            float: 'left'
-        };
         if (this.props.data === undefined) {
             return (
                 <div>
                     <p>Loading...</p>
-                    </div>
+                </div>
             );
         }
         else {
             return (
-                <div className="nineties_img_item">
+                <div className="nineties_img_item clear_floats">
+
+                    <img src={this.props.data.url} width={225} height={400}/>
+
+                    <div className="nineties_img_item_scroll_container"
+                         ref="scrollBoxContainer">
+
+                        <section className="nineties_img_item_scroll">
+
+                            <h1>{this.props.data.title}</h1>
+
+                            <span className="nineties_img_item_description"
+                                dangerouslySetInnerHTML={{__html: this.props.data.description}}/>
+
+                            <div className="mac_os8_button">
+                                <Link to={AppRoutes.NINETIES_IMG_INDEX}>View
+                                    More...</Link>
+                            </div>
+                        </section>
+                    </div>
+
+
                     <div className="nineties_img_navigator">
                         <a className="nineties_img_nav_button mac_os8_sprites previous"
-                            onClick={this.handleClick.bind(this, 'prev')} />
+                           onClick={this.handleClick.bind(this, 'prev')}/>
+
                         <div className="nineties_img_nav_button counter">
                             <span>{this.props.data.pk}</span>
                             <span>/</span>
                             <span>{this.props.dataCount}</span>
                         </div>
                         <a className="nineties_img_nav_button mac_os8_sprites next"
-                             onClick={this.handleClick.bind(this, 'next')} />
-                    </div>
-
-                    <img src={this.props.data.url} height={400} style={style}/>
-
-                    <h1>{this.props.data.title}</h1>
-
-                    <span className="nineties_img_item_description"
-                         dangerouslySetInnerHTML={{__html: this.props.data.description}} />
-
-                    <div className="mac_os8_button">
-                        <Link to={AppRoutes.NINETIES_IMG_INDEX}>View More...</Link>
+                           onClick={this.handleClick.bind(this, 'next')}/>
                     </div>
                 </div>
             );
